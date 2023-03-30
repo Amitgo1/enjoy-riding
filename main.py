4f82ce5c0c3a18fba8e5adf9ff15f627c8ff1e3f
Amitgo1 <amitgoswami089@gmail.com>
2023-03-30 19:06:04 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template,redirect
+
+app =Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return redirect("/")
+
+app.run(debug=True)
