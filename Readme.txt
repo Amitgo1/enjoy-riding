65649906ef7c5b3209b9a43298c182ce5950594f
Amitgo1 <amitgoswami089@gmail.com>
2023-04-02 11:37:42 +0530

    changes

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+please use python version : 3.11.1
+requirements.txt file create karne ke liye humko yeh 
+command chalani hai venv activate karo
+fir likho pip freeze > requirements.txt
+update karne ke liye fir se vo hi command chalao  
+agar aapko yeh module install karne hai toh ek command chalao
+pip install -r requirements.txt 
--- main.py
+++ main.py
@@ -10,4 +10,40 @@ def main():
 def home():
     return redirect("/")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/service")
+def service():
+    return render_template("service.html")
+
+@app.route("/package")
+def package():
+    return render_template("package.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/blog")
+def blog_grid():
+    return render_template("blog.html")
+
+@app.route("/single")
+def blog_detail():
+    return render_template("single.html")
+
+@app.route("/destination")
+def destination():
+    return render_template("destination.html")
+
+@app.route("/guide")
+def travel_guide():
+    return render_template("guide.html")
+
+@app.route("/testimonial")
+def test():
+    return render_template("testimonial.html")
+
 app.run(debug=True)
